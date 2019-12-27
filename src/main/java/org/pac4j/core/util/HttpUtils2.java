470f70ed71aa05d44e3bd3ddb5654640a955d37f
wandl <hnxyhcwdl1003@163.com>
2019-12-27 16:26:10 +0800

    修改版权信息

--- src/main/java/org/pac4j/core/util/HttpUtils2.java
+++ src/main/java/org/pac4j/core/util/HttpUtils2.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, vindell (https://github.com/vindell).
+ * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -43,7 +43,7 @@ public class HttpUtils2 {
 	
 	/**
 	 * 在一次请求发送后获取SessionID
-	 * @author 		： <a href="https://github.com/vindell">vindell</a>
+	 * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
 	 * @param httpConn HttpURLConnection对象
 	 * @return
 	 */
